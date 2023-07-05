bdd54c56d2fdb421db16587191c776fe1797b6bb
Nika <nika.nishnianidze.1@iliauni.edu.ge>
2023-07-06 01:21:04 +0400

    added accrodions for questions on homepage

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,15 +1,15 @@
 window.addEventListener('load', () => {
-	const questions = Array.from(document.querySelectorAll('.question'));
+	const questions = Array.from(document.querySelectorAll('.about_div'));
 
 	questions.forEach(question => {
-		const answer = question.querySelector('.answer');
+		const answer = question.querySelector('.community_text');
 		const scrollHeight = answer.scrollHeight;
 
 		answer.style.setProperty('--height', `${scrollHeight}px`)
 
 		
 		question.addEventListener('click', () => {
-			question.classList.toggle('active');
+			question.classList.toggle('toggled');
 		});
 	});
 });
